022529e00637cce3f78f119764884667cdb733d4
Parksungchan1 <parksungchan3775@gmail.com>
2026-06-08 18:29:28 +0900

    fix: KakaoMapSdk 초기화 키 수정 (401 오류 해결)

--- app/src/main/java/com/example/projectlast/MyApplication.java
+++ app/src/main/java/com/example/projectlast/MyApplication.java
@@ -10,7 +10,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         try {
-            KakaoMapSdk.init(this, "09dec9415b32086dda0d2a5ececd931e");
+            KakaoMapSdk.init(this, "6bd533a92e9f03be75ce8bd95aa0b055");
             kakaoMapAvailable = true;
         } catch (Throwable t) {
             // x86_64 에뮬레이터에서는 ARM 전용 네이티브 라이브러리 로드 불가
